da01abcc6729566314047f488d166e5472a50d31
Joe Shepherd <sudo.jshepherd01@gmail.com>
2023-11-04 20:17:08 +0000

    feat: topic endpoint, and refactor login

--- server.py
+++ server.py
@@ -2,9 +2,12 @@ from flask import Flask, request, send_file
 from pathlib import Path
 import json
 
+import model
+
 OK = 200
 CREATED = 201
 INVALID = 400
+NOT_FOUND = 404
 
 CONTENT_HTML = {'ContentType':'text/html'}
 CONTENT_PLAIN = {'ContentType':'text/plain'}
@@ -21,6 +24,22 @@ def require_body_parameters(request, params):
             return False, f"No {param} provided"
     return True, None
 
+
+def get_user_data(request):
+    username = request.json["username"]
+    user_file = data_dir / f"{username}.json"
+    if not user_file.exists: return None
+    with open(user_file, "r") as f:
+        d = json.load(f)
+    return d
+
+def save_user_data(request, user_data):
+    username = request.json["username"]
+    user_file = data_dir / f"{username}.json"
+    if not user_file.exists: return None
+    with open(user_file, "w") as f:
+        json.dump(user_data, f)
+
 @app.route("/")
 def serve_index():
     return send_file("static/index.html"), OK, CONTENT_HTML
@@ -32,13 +51,27 @@ def login():
     if not valid:
         return reason, INVALID, CONTENT_PLAIN
 
-    username = request.json["username"]
-    user_file = data_dir / f"{username}.json"
-    if not user_file.exists():
-        with open(user_file, "w") as f:
+    user_data = get_user_data(request)
+    if user_data is None:
+        username = request.json["username"]
+        with open(data_dir / f"{username}.json", "w") as f:
             f.write("{}")
         return [], CREATED, CONTENT_JSON
-    
-    with open(user_file, "r") as f:
-        user_data = json.load(f)
     return list(user_data.keys()), OK, CONTENT_JSON
+
+@app.route("/topic", methods=["POST"])
+def topic():
+    valid, reason = require_body_parameters(request, ["username", "topic"])
+    if not valid:
+        return reason, INVALID, CONTENT_PLAIN
+    
+    user_data = get_user_data(request)
+    if user_data is None:
+        return "User not registered", NOT_FOUND, CONTENT_PLAIN
+    
+    topic = request.json["topic"]
+    if not topic in user_data:
+        subtopics = model.query_topic(topic)
+        user_data[topic] = {subtopic: {} for subtopic in subtopics}
+        save_user_data(request, user_data)
+    return list(user_data[topic].keys()), OK, CONTENT_JSON
