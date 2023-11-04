ff30bc2f7275b405a9605958eb5f10a5fa44451f
JoshuaJackNewbouldcd <JoshuaNewbould@outlook.com>
2023-11-04 21:41:03 +0000

    fixed summariser

--- summariser.py
+++ summariser.py
@@ -1,12 +1,13 @@
 import os
 import openai
 
-summary_prompt = 'summarise the following dialogue in detail for at least 30 words, but no more than 60 words, then give a list of topics of what the user and the assistant talked about. Example topics discussed can be equations, history facts, etc.' 
-comb_prompt = "The user and the teacher previously talked about {} They also talked about {}\nPlease summarise the given information above in detail but less than 200 words. Then give a list of topic they've talked about. Example topics discussed can be equations, history facts, etc."
+summary_prompt = 'summarise the following dialogue in detail for at least 30 words, but no more than 60 words {}. Now give a list of topics of what the user and the assistant talked about. Example topics discussed can be equations, history facts, introductions etc. ' 
+comb_prompt = "The user and the teacher previously talked about {} They also talked about {}\nPlease summarise the given information above in detail but less than 200 words. Then give a list of topic they've talked about. Example topics discussed can be equations, history facts, etc. "
 
 
 def summarise(B):
-    n_prompt = summary_prompt + B
+    n_B = ', '.join(B)
+    n_prompt = summary_prompt.format(n_B)
     return send(n_prompt)
 
 def combine(A, B):
@@ -14,7 +15,7 @@ def combine(A, B):
     return send(n_prompt)
 
 def batched(conv):
-    batch_max = 20
+    batch_max = 100
     batches = []
     curr_batch = []
     size = 0
@@ -27,6 +28,7 @@ def batched(conv):
             curr_batch = []
             curr_batch += [msg]
             size = len(msg)
+    batches += [[*curr_batch]]
     return batches
 
 def main(conv):
@@ -46,9 +48,11 @@ def send(message):
     model="local-model", # this field is currently unused
     messages=[
         {"role": "user", "content": message}
-    ]
+    ],
+    temperature = 0.1,
+    top_p = 0.5
     )
 
     print(completion.choices[0].message)
 
-main(["hello, my name is josh", "hi josh, my name is chatgpt", "nice to meet you chat gpt, i hope we have good conversations"])
+main(["User: hello, my name is josh", "AI: hi josh, my name is chatgpt", "User: nice to meet you chat gpt, i hope we have good conversations"])
