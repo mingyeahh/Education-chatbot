9307f029aa7bb315189865a54d552921eb7c4ff6
Joe Shepherd <sudo.jshepherd01@gmail.com>
2023-11-04 19:51:29 +0000

    refactor: extract validation code to function

--- server.py
+++ server.py
@@ -13,6 +13,14 @@ CONTENT_JSON = {'ContentType':'application/json'}
 app = Flask(__name__, static_url_path='', static_folder="static")
 data_dir = Path("data")
 
+def require_body_parameters(request, params):
+    if request.content_type != "application/json":
+        return False, "Body data must be JSON"
+    for param in params:
+        if not param in request.json or request.json[param] == "":
+            return False, f"No {param} provided"
+    return True, None
+
 @app.route("/")
 def serve_index():
     return send_file("static/index.html"), OK, CONTENT_HTML
@@ -20,10 +28,9 @@ def serve_index():
 @app.route("/login", methods=["POST"])
 def login():
     """Incredibly insecure login function."""
-    if request.content_type != "application/json":
-        return "Body data must be JSON", INVALID, CONTENT_PLAIN
-    if not "username" in request.json or request.json["username"] == "":
-        return "No username provided", INVALID, CONTENT_PLAIN
+    valid, reason = require_body_parameters(request, ["username"])
+    if not valid:
+        return reason, INVALID, CONTENT_PLAIN
 
     username = request.json["username"]
     user_file = data_dir / f"{username}.json"
