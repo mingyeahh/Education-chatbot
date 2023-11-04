d07df1ef5f751ff882fc3a801095345834f8ecef
Joe Shepherd <sudo.jshepherd01@gmail.com>
2023-11-04 21:11:20 +0000

    feat: subtopic endpoint

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
 def query_topic(topic):
     # mock implementation
     return [i for i in topic]
+
+def query_subtopic_start(subtopic, **kwargs):
+    # mock implementation
+    return {"sender": "assistant", "content": f"The topic of {subtopic} can be thought of as a branch of graph theory."}
