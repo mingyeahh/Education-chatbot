6bc851be2dae145a62c56f6cf6aa2aa3f2bc5908
Mingyue Jian <71535426+mingyeahh@users.noreply.github.com>
2023-11-05 10:41:27 +0000

    fix: debug summariser

--- summariser.py
+++ summariser.py
@@ -1,12 +1,12 @@
 import os
 import openai
 
-summary_prompt = "summarise the following dialogue in detail for at least 30 words, but no more than 60 words {}. Now give a list of topics of what the user and the assistant talked about. Example topics discussed can be equations, history facts, introductions etc. "
-comb_prompt = "The user and the teacher previously talked about {} They also talked about {}\nPlease summarise the given information above in detail but less than 200 words. Then give a list of topic they've talked about. Example topics discussed can be equations, history facts, etc. "
+summary_prompt = "summarise the following dialogue in detail for at least 50 words, but no more than 200 words {}."
+comb_prompt = "The user and the teacher previously talked about {} They also talked about {}\nPlease summarise the given information above in detail but less than 200 words. "
 top_to_sub_prompt = "give me a numbered list of subtopics related to: {}"
 lesson_prompt = "You are a teacher, teach me a full lesson on {}, with regards to {}. Ask me a question to test my knowledge at the end"
 standard_prompt = "You are a teacher, we are learning {} with respect to {}. {} Remember the summary, topic, subtopic, and your role as a teacher"
-SUMMARY_AMOUNT = 1000
+SUMMARY_AMOUNT = 2000
 
 
 def top_to_sub(topic):
@@ -22,12 +22,12 @@ def lesson(topic, subtopic):
 def summarise(B):
     n_B = ", ".join(B)
     n_prompt = summary_prompt.format(n_B)
-    return send(n_prompt)
+    return send(n_prompt)["content"]
 
 
 def combine(new, old):
     n_prompt = comb_prompt.format(old, new)
-    return send(n_prompt)
+    return send(n_prompt)["content"]
 
 
 def batched(conv):
@@ -59,32 +59,36 @@ def create_summary(conv):
 
 
 def send_message(message, topic, subtopic, data):
-    summary = ("The summary of the past is " + data["summary"]) if data["summary"] is not None else ""
-    n_prompt = standard_prompt.format(topic, subtopic, summary)
     summ_form = []
     size = 0
     i = data["summ_index"] if "summ_index" in data else 0
-    the_summary = None
-    the_increment = None
+    the_summary = data["next_summary"]
+    # print(the_summary)
     for msg in data["conversation"][i:]:
         comb_msg = "{}: {}".format(msg["role"], msg["content"])
         summ_form.append(comb_msg)
         size += len(comb_msg)
+        # print(summ_form)
         if size >= SUMMARY_AMOUNT:
-            if the_summary is not None:
+            # print("SUMMARISING")
+            if the_summary != "":
+                # print(f"combining {the_summary} and {data['summary']}")
                 comb_summ = combine(the_summary, data["summary"])
                 data["summary"] = comb_summ
-                data["summ_index"] += the_increment
+            # print("summarising")
             the_summary = summarise(summ_form)
-            the_increment = len(summ_form)
+            data["summ_index"] += len(summ_form)
             summ_form = []
             size = 0
     data["next_summary"] = the_summary
+
+    summary = ("The summary of the past is " + data["summary"]) if data["summary"] != "" else ""
+    n_prompt = standard_prompt.format(topic, subtopic, summary)
     messages = [
         {"role": "system", "content": n_prompt},
     ] + data[
         "conversation"
-    ][data["summ_index"]:]
+    ][data["summ_index"] :]
     return send(message, past_messages=messages)
 
 
